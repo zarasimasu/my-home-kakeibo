6cb3d0e87f84fa078a72647fca7a2b34fc7dc67d
zarasimasu <ouz4r83y325332g@gmail.com>
2020-10-06 14:50:30 +0900

    バリデーションの追加

--- app/models/fixedcost.rb
+++ app/models/fixedcost.rb
@@ -1,2 +1,10 @@
 class Fixedcost < ApplicationRecord
+  validates :fixedcost_category_id, numericality: { other_than: 1 }
+  validates :value, format: { with: /\A[0-9]+\z/ }
+
+  with_options presence: true do
+    validates :value, :year_month
+  end
+
+  belongs_to :user
 end
