f90afb4ad678a52e332e45009cc4a00992ef89c3
zarasimasu <ouz4r83y325332g@gmail.com>
2020-10-07 15:43:18 +0900

    createアクションの実装

--- app/controllers/variablecosts_controller.rb
+++ app/controllers/variablecosts_controller.rb
@@ -15,12 +15,24 @@ class VariablecostsController < ApplicationController
 	def edit
 	end
  
-	def create
+  def create
+    @variablecost = Variablecost.new(variablecost_params)
+		if @variablecost.save
+			redirect_to variablecosts_path
+		else
+			render :new
+		end
 	end
  
 	def update
 	end
  
 	def destroy
+  end
+  
+  private
+
+	def variablecost_params
+		params.require(:variablecost).permit(:variablecost_category_id, :value, :year_month, :description).merge(user_id: current_user.id)
 	end
 end
