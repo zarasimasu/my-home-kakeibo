b799736d8048ece3f5902ce50833f0f015531345
zarasimasu <ouz4r83y325332g@gmail.com>
2020-10-07 11:43:14 +0900

    固定費情報編集画面の表示

--- app/controllers/fixedcosts_controller.rb
+++ app/controllers/fixedcosts_controller.rb
@@ -13,6 +13,11 @@ class FixedcostsController < ApplicationController
 	end
  
 	def edit
+		@fixedcost = Fixedcost.find(params[:id])
+		if @fixedcost.user_id == current_user.id
+		else
+			redirect_to root_path
+		end
 	end
  
 	def create
