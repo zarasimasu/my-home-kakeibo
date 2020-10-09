99852d211dd47e80f8852c04f476f6fd0b537f67
zarasimasu <ouz4r83y325332g@gmail.com>
2020-10-09 15:41:22 +0900

    月次収入合計の表示

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -6,7 +6,14 @@ class BalancesController < ApplicationController
 
   def show
     @year_month = params[:year_month]
+    
+    @incomes = current_user.incomes.where(year_month: (@year_month + "-01"))
+
     @income = current_user.incomes.where(year_month: (@year_month + "-01")).pluck(:value)
+    @income_total = 0
+    @income.each do |income|
+      @income_total += income
+    end
   end
 
 end
