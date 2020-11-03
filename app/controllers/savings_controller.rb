fc5d4ad44d66537d816e5d90d500906157e1848b
zarasimasu <ouz4r83y325332g@gmail.com>
2020-11-03 10:24:05 +0900

    貯金額登録画面への遷移

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -2,6 +2,14 @@ class SavingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @savings = current_user.savings.all
   end
-  
+
+  def new
+    @saving = Saving.new
+  end
+
+  def create
+  end
+
 end
