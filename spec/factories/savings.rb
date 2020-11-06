0a810560f94e5722a21a4679321f6cde0e57909d
zarasimasu <ouz4r83y325332g@gmail.com>
2020-11-06 16:50:35 +0900

    savingモデルの単体テストコード

--- spec/factories/savings.rb
+++ spec/factories/savings.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :saving do
-    
+    day {2020-01-01}
+    saving {10000}
+    association :user
   end
 end
--- spec/models/saving_spec.rb
+++ spec/models/saving_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Saving, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @saving = build(:saving)
+  end
+
+  describe "バリデーション" do
+
+    it "すべての項目が入力されていれば登録できる" do
+      expect(@saving).to be_valid
+    end
+
+    it "日付が入力されていなければ登録できない" do
+      @saving.day = nil
+      @saving.valid?
+      expect(@saving.errors.full_messages).to include("Day can't be blank")
+    end
+
+    it "金額が入力されていなければ登録できない" do
+      @saving.saving = nil
+      @saving.valid?
+      expect(@saving.errors.full_messages).to include("Saving is invalid", "Saving is not a number", "Saving can't be blank")
+    end
+
+    it "金額がマイナスだと登録できない" do
+      @saving.saving = -1
+      @saving.valid?
+      expect(@saving.errors.full_messages).to include("Saving is invalid", "Saving must be greater than or equal to 0")
+    end
+
+    it "金額の入力が半角数字以外だと登録できない" do
+      @saving.saving = "１００００"
+      @saving.valid?
+      expect(@saving.errors.full_messages).to include("Saving is not a number")
+    end
+  end
 end
